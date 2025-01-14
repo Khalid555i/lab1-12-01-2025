217af7681f2edbd27244798fc98205d9613d9d83
Khalid555i <102835152+Khalid555i@users.noreply.github.com>
2025-01-14 11:40:10 +0600

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,28 @@
+/*
+Date: 14-01-2025
+Name: M. K. KHALID MAHAMUD
+Dept. of CSE Varendra,University
 
+Problem 1: To Display n number of natural numbers and their sum. 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n,sum = 0;
+  cout << "Enter the Numbers of Natural Numbers : " ;
+
+  cout << "The first " << n <<" natural number is : ";
+
+  for(int i=0; i<n; i++)
+    {
+      cout << i << " ";
+      sum += i;
+    }
+
+  cout << n << " Natural numbers sum is :" << sum ;
+
+  return 0;
+}
